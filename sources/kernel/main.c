6680e508f9715a08a12e2b55edb51008454971d3
x7k3e21 <uly4nkin.denis@gmail.com>
2023-09-03 15:51:20 +0300

    Removed unused kernel C code

--- sources/kernel/main.c
+++ sources/kernel/main.c
@@ -1,9 +1,6 @@
 
-#if defined(__linux__)
-    #error "You are not using a cross-compiler, this code may be compiled wrong!" 
-#endif
 
-void kernel_entry(void)
+void _kernel_entry(void)
 {
-    
+
 }
