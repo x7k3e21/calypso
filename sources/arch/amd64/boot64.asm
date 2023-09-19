4431eb1a651bd78dc09f5652e6cd83536567114c
x7k3e21 <uly4nkin.denis@gmail.com>
2023-09-19 16:36:01 +0300

    Several typo fixes

--- sources/arch/amd64/boot64.asm
+++ sources/arch/amd64/boot64.asm
@@ -1,12 +1,14 @@
 
+    ; This code will be running in Long Mode (64-bit)
     [bits 64]
 
+    ; Declaring TEXT section, where we will put our instructions
     section .text
 
     global _lm_bootloader_entry
 
 _lm_bootloader_entry:
-    mov ax, 0
+    xor ax, ax
     mov ss, ax
     mov ds, ax
     mov es, ax
